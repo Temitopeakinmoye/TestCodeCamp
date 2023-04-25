ece2e27fafc0e560c626a8edaf040db0b0fbc34c
Temitopeakinmoye <raphaelaomoakin4@gmail.com>
2023-04-25 13:14:42 +0100

    Task 14 firtsjavaproject

---
+++ FirstJavaProject/src/Main.java
@@ -0,0 +1,7 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World! This is my new project");
+        System.out.println("my first JAVA code, \n i hope the java experience will be a nice one\n and i hope intellij too will be sweet.\n lastly, \n next week is for individual project \n and i pray and hope \n it will be victory at last");
+    }
+}
